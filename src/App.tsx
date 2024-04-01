19de4a686d0a1c3a2bbb4c3183eec983a8acad95
Parviz Shirinov <parveeezy@gmail.com>
2024-04-01 15:22:13 +0300

    styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import {Counter} from "./components/Counter/Counter";
 import {CounterSetter} from "./components/CounterSetter/CounterSetter";
 
 function App() {
 
+    const [maxValue, setMaxValue] = useState(0);
+    const [startValue, setStartValue] = useState(0)
+
     return (
         <div className={'appWrapper'}>
             <div className={'container'}>
                 <div>
-                    <CounterSetter/>
+                    {/*<CounterSetter*/}
+                    {/*    setMaxValue={setMaxValue}*/}
+                    {/*    setStartValue={setStartValue}*/}
+                    {/*/>*/}
                 </div>
                 <div>
-                    <Counter/>
+                    <Counter
+                        maxValue={maxValue}
+                        startValue={startValue}
+                    />
                 </div>
             </div>
         </div>
